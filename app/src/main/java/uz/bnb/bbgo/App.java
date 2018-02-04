de1053db5c82156f2f6b3baf37c04f4ed37135b9
mirsaidoff <mirsaidoff.n@gmail.com>
2018-02-04 19:20:49 +0500

    main

---
+++ app/src/main/java/uz/bnb/bbgo/App.java
@@ -0,0 +1,15 @@
+package uz.bnb.bbgo;
+
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class App extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
